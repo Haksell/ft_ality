7f6ab28f20e6d57a9b27228137218731326453d7
Axel Brisse <axel.brisse@gmail.com>
2024-04-09 05:46:41 +0200

    start parseFile

--- srcs/main.hs
+++ srcs/main.hs
@@ -3,9 +3,16 @@
 import Args (Args (..), parseAndValidateArgs)
 import Colors (Color (..), putColorful)
 
+parseFile :: FilePath -> IO (String, String)
+parseFile filePath = do
+  content <- readFile filePath
+  putColorful Green content
+  return ("keymap", "combos")
+
 main :: IO ()
 main = do
   args <- parseAndValidateArgs
   putColorful Red $ if argDebug args then "debug" else "quiet"
-  fileContents <- readFile $ argFilename args
-  putColorful Green fileContents
+  (keymap, combos) <- parseFile (argFilename args)
+  putColorful Green keymap
+  putColorful Blue combos
