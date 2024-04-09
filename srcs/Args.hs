1c36cda3fc96a8ef6c5aeb29694e569e8588a226
Axel Brisse <axel.brisse@gmail.com>
2024-04-09 05:36:29 +0200

    module Args

---
+++ srcs/Args.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Args (Args (..), parseAndValidateArgs) where
+
+import Data.List (isSuffixOf)
+import Options.Applicative
+  ( Parser,
+    argument,
+    execParser,
+    fullDesc,
+    help,
+    helper,
+    info,
+    long,
+    metavar,
+    short,
+    str,
+    switch,
+    (<**>),
+  )
+import System.Exit (ExitCode (ExitFailure), exitWith)
+
+data Args = Args
+  { argFilename :: String,
+    argDebug :: Bool
+  }
+
+parseArgs :: Parser Args
+parseArgs =
+  Args
+    <$> argument str (metavar "grammar.gmr" <> help "Description of the keymap and combos")
+    <*> switch (long "debug" <> short 'd' <> help "Debug mode")
+
+validateArgs :: Args -> IO Args
+validateArgs args =
+  if ".gmr" `isSuffixOf` argFilename args
+    then return args
+    else do
+      putStrLn "Error: the file path must end with '.gmr'."
+      exitWith (ExitFailure 1)
+
+parseAndValidateArgs :: IO Args
+parseAndValidateArgs = do
+  args <- execParser $ info (parseArgs <**> helper) fullDesc
+  validateArgs args
--- srcs/main.hs
+++ srcs/main.hs
@@ -1,51 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Args (Args (..), parseAndValidateArgs)
 import Colors (Color (..), putColorful)
-import Data.List (isSuffixOf)
-import Options.Applicative
-  ( Parser,
-    argument,
-    execParser,
-    fullDesc,
-    help,
-    helper,
-    info,
-    long,
-    metavar,
-    short,
-    str,
-    switch,
-    (<**>),
-  )
-import System.Exit (ExitCode (ExitFailure), exitWith)
-
-data Args = Args
-  { argFilename :: String,
-    argDebug :: Bool
-  }
-
-parseArgs :: Parser Args
-parseArgs =
-  Args
-    <$> argument str (metavar "grammar.gmr" <> help "Description of the keymap and combos")
-    <*> switch (long "debug" <> short 'd' <> help "Debug mode")
-
-validateArgs :: Args -> IO Args
-validateArgs args =
-  if ".gmr" `isSuffixOf` argFilename args
-    then return args
-    else do
-      putStrLn "Error: the file path must end with '.gmr'."
-      exitWith (ExitFailure 1)
-
-parseAndValidateArgs :: IO Args
-parseAndValidateArgs = do
-  args <- execParser $ info (parseArgs <**> helper) fullDesc
-  validateArgs args
 
 main :: IO ()
 main = do
-  validArgs <- parseAndValidateArgs
-  putStrLn $ if argDebug validArgs then "debug" else "quiet"
-  fileContents <- readFile $ argFilename validArgs
+  args <- parseAndValidateArgs
+  putColorful Red $ if argDebug args then "debug" else "quiet"
+  fileContents <- readFile $ argFilename args
   putColorful Green fileContents
