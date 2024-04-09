247a6dd731bd381dd4443b8dbb0038c533157c20
Axel Brisse <axel.brisse@gmail.com>
2024-04-09 05:56:44 +0200

    bring back character input

--- srcs/main.hs
+++ srcs/main.hs
@@ -2,6 +2,14 @@
 
 import Args (Args (..), parseAndValidateArgs)
 import Colors (Color (..), putColorful)
+import Control.Monad (when)
+import Data.Char (isDigit, isLower, isUpper, toUpper)
+import System.IO
+  ( BufferMode (NoBuffering),
+    hSetBuffering,
+    hSetEcho,
+    stdin,
+  )
 
 type ParsedContent = (String, String)
 
@@ -13,6 +21,30 @@ parseFile filePath = do
   content <- readFile filePath
   return $ parseContent content
 
+displayColored :: Char -> IO ()
+displayColored char
+  | isDigit char = putColorful Yellow [char]
+  | isUpper char = putColorful Red [char]
+  | isLower char = putColorful Red [toUpper char]
+  | otherwise = return ()
+
+loop :: IO ()
+loop = do
+  char <- getChar
+  case char of
+    '\ESC' -> do
+      next <- getChar
+      when (next == '[') $ do
+        direction <- getChar
+        case direction of
+          'A' -> putColorful Green "[Up]"
+          'B' -> putColorful Green "[Down]"
+          'C' -> putColorful Green "[Right]"
+          'D' -> putColorful Green "[Left]"
+          _ -> return ()
+    _ -> displayColored char
+  loop
+
 main :: IO ()
 main = do
   args <- parseAndValidateArgs
@@ -20,3 +52,6 @@ main = do
   (keymap, combos) <- parseFile (argFilename args)
   putColorful Green keymap
   putColorful Blue combos
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+  loop
