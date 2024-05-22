e32b1ffc1823b79a076ef4cc4462017d4bca2443
Axel Brisse <axel.brisse@gmail.com>
2024-05-22 05:37:10 +0200

    .

--- srcs/DFA.hs
+++ srcs/DFA.hs
@@ -1,7 +1,7 @@
 module DFA (Combo (..), printCombos, printSuccessfulCombo, parseDFA) where
 
 import Colors (Color (..), colored, putColorful)
-import Control.Monad (unless, when)
+import Control.Monad (when)
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
@@ -38,8 +38,8 @@ data DFA = DFA
 type ComboCache = Set.Set (String, String)
 
 -- TODO: only return Combo
-parseCombo :: String -> Set.Set String -> ComboCache -> IO (ComboCache, Combo)
-parseCombo comboLine possibleActions cache = do
+parseCombo :: String -> Set.Set String -> IO Combo
+parseCombo comboLine possibleActions = do
   let parts = splitOn "/" comboLine
   case parts of
     [actionsStr, name, fighter] -> do
@@ -49,24 +49,23 @@ parseCombo comboLine possibleActions cache = do
       when ("" `elem` actions) $ panic ("Empty action for combo " ++ representation)
       when (any (`Set.notMember` possibleActions) actions) $
         panic ("Unknown action for combo " ++ representation) -- TODO: say which one
-      let comboIdentity = (name, fighter)
-      when (Set.member comboIdentity cache) $ panic ("Duplicate combo: " ++ representation)
       return
-        ( Set.insert comboIdentity cache
-        , Combo
-            { comboName = colored Blue name
-            , comboFighter = colored Red fighter
-            , comboActions = actions
-            , comboLen = length actions
-            }
-        )
+        Combo
+          { comboName = colored Blue name
+          , comboFighter = colored Red fighter
+          , comboActions = actions
+          , comboLen = length actions
+          }
     _ -> panic "Combo line should be in the following format: actions/name/fighter"
 
 parseCombos :: [String] -> Set.Set String -> ComboCache -> IO [Combo]
 parseCombos [] _ _ = return []
 parseCombos (comboLine : comboLines) possibleActions cache = do
-  (newCache, combo) <- parseCombo comboLine possibleActions cache
-  combos <- parseCombos comboLines possibleActions newCache
+  combo <- parseCombo comboLine possibleActions
+  let comboIdentity = (comboName combo, comboFighter combo)
+  when (Set.member comboIdentity cache) $
+    panic ("Duplicate combo: " ++ comboName combo ++ " (" ++ comboFighter combo ++ ")")
+  combos <- parseCombos comboLines possibleActions (Set.insert comboIdentity cache)
   return (combo : combos)
 
 parseDFA :: [String] -> Set.Set String -> IO DFA
