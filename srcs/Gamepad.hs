b681b7893b60cde1329e7013489ce6705f04ca44
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 06:04:28 +0200

    clean gamepad a bit

--- srcs/Gamepad.hs
+++ srcs/Gamepad.hs
@@ -1,11 +1,10 @@
-{-# OPTIONS_GHC -Wno-unused-do-bind #-}
-
 module Gamepad (initGameContoller, getActionGamepad) where
 
+import Control.Monad (void)
 import Data.Char (toUpper)
 import qualified Data.Map as Map
 import Data.Maybe (catMaybes)
-import qualified Data.Vector as V
+import qualified Data.Vector as V -- TODO: directly SDL vector?
 import Keymap (Keymap)
 import SDL (
   ControllerButtonEventData (controllerButtonEventButton, controllerButtonEventState),
@@ -25,15 +24,10 @@ import Utils (panic)
 initGameContoller :: IO ()
 initGameContoller = do
   SDL.initialize [SDL.InitGameController]
-
   controllers <- availableControllers
   if V.null controllers
-    then
-      panic "No game controllers connected!"
-    else do
-      let firstController = V.head controllers
-      openController firstController
-      return ()
+    then panic "No game controllers connected!"
+    else void (openController (V.head controllers))
 
 getActionGamepad :: Keymap -> IO [String]
 getActionGamepad keymap = do
