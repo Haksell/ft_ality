407bcb4d1e9359adee9954f97b3becb015997b43
Haksell <axel.brisse@gmail.com>
2024-09-12 02:58:06 +0200

    clean keymap

--- srcs/Keymap.hs
+++ srcs/Keymap.hs
@@ -36,30 +36,30 @@ parseKeymap keymapSection = do
   case keymapOrDuplicate of
     Left keymap -> return keymap
     Right duplicate -> panic $ "Duplicate key found: " ++ duplicate
-
-buildKeymap :: [(String, String)] -> IO (Either Keymap String)
-buildKeymap = f Map.empty
  where
-  f keymap [] = return $ Left keymap
-  f keymap ((k, a) : xs) =
+  parseMapping :: String -> IO (String, String)
+  parseMapping keymapLine = do
+    let parts = splitOn "/" keymapLine
+    case parts of
+      [key, action] -> return (key, action)
+      _ -> panic "Action line should be in the following format: key/action"
+
+  buildKeymap :: [(String, String)] -> IO (Either Keymap String)
+  buildKeymap = buildKeymap' Map.empty
+
+  buildKeymap' keymap [] = return $ Left keymap
+  buildKeymap' keymap ((k, a) : xs) =
     if Map.member k keymap
       then return $ Right k
       else do
         checkedKey <- checkKey $ map toUpper k
-        f (Map.insert checkedKey a keymap) xs
+        buildKeymap' (Map.insert checkedKey a keymap) xs
 
   checkKey :: String -> IO String
   checkKey k =
     if length k == 1 && isAsciiLetter (head k) || k `elem` validKeys || k `elem` validButtons
       then return k
       else panic $ "Invalid key: " ++ k
-
-parseMapping :: String -> IO (String, String)
-parseMapping keymapLine = do
-  let parts = splitOn "/" keymapLine
-  case parts of
-    [key, action] -> return (key, action)
-    _ -> panic "Action line should be in the following format: key/action"
 
 printKeymap :: Keymap -> Bool -> IO ()
 printKeymap keymap gamepad = do
